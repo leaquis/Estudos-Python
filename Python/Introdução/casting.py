492701ba25b5a47055739616e85f1175625ee7d3
Giovani da Silva Ramos <gi_1997@hotmail.com>
2025-03-19 02:35:16 -0300

    casting e type

---
+++ Python/Introdução/casting.py
@@ -0,0 +1,36 @@
+#int
+
+x = int(2)
+y = int(2.8)
+z = int('2')
+
+print(x, y, z)
+
+#float
+
+a = float(2.3)
+b = float(2)
+c = float('2.3')
+d = float('2')
+
+print(a, b, c, d)
+
+#str
+
+print("Sem conversao para string:")
+g = 's1'
+h= 2
+i = 2.3
+
+print("A variavel G e do tipo: ", type(g))
+print("A variavel H e do tipo: ", type(h))
+print("A variavel I e do tipo: ", type(i))
+
+print("Conversao para string:")
+g = str('s1')
+h= str(2)
+i = str(2.3)
+
+print("A variavel G e do tipo: ", type(g))
+print("A variavel H e do tipo: ", type(h))
+print("A variavel I e do tipo: ", type(i))
