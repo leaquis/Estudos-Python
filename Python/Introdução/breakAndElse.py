7d36a9196a86fee67c4ec963929485e508acaf14
Giovani da Silva Ramos <gi_1997@hotmail.com>
2025-03-23 03:54:21 -0300

    estruturas de repetição

---
+++ Python/Introdução/breakAndElse.py
@@ -0,0 +1,9 @@
+a = 0
+
+while a <= 10:
+    print(a)
+    """if a == 3:
+        break"""
+    a += 1
+else:
+    print(f"A nao e menor ou igual a 10. Valor de a = {a}")
---
+++ Python/Introdução/forAndRange.py
@@ -0,0 +1,19 @@
+"""
+s = "pernambuco"
+
+for x in s:
+    print(x)
+"""
+
+"""
+for x in range(6):
+    print(x)
+"""
+
+"""
+for x in range(5, 8):
+    print(x)
+"""
+
+for x in range(2, 13, 5):
+    print(x)
